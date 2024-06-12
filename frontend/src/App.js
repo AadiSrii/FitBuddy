6ce0b2d462fc6d0fab8b51227ffbffe3ba228863
Vibha Maurya <vibhamaurya060@gmail.com>
2024-06-12 23:27:57 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import { Home } from "./pages/Home";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
-const Navbar=() =>{
+export const Navbar = () => {
     return (
-        
+        <div style={{ display: "flex", gap: "40px", borderBottom: "1px solid black", alignItems: "center", justifyContent: "center", backgroundColor: "black" }}>
+            <h2 style={{ color: "white" }}>FitBuddy</h2>
+            <Link to="/">Home</Link>
+        </div>
     )
 }
