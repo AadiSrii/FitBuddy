8f4d00a8ac1e0a03aacee9e673ca37f66fa94543
Vibha Maurya <vibhamaurya060@gmail.com>
2024-06-14 22:59:58 +0530

    day4

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,6 +11,7 @@ export const Navbar = () => {
             <Link to="/Shop" style={{textDecoration:"none", color:"white", fontSize:"16px", marginLeft:"10px"}}>Shop</Link>
             <Link to="/Login" style={{textDecoration:"none", color:"white", fontSize:"16px", marginLeft:"200px"}}><button style={{width:"80px",height:"42px",fontWeight:"800", fontSize:"14px"}}>LOG IN</button></Link>
             <Link to="/Signup" style={{textDecoration:"none", color:"white", fontSize:"16px"}}><button style={{width:"100px",height:"42px",fontWeight:"800", fontSize:"14px"}}>SIGN UP</button></Link>
+            
         </div>
     );
 };
