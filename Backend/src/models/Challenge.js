24e235e4ea4fdf1d81dc5be5ed8c04f6e1fbf465
bernard powers <adarshsrivastava0348@gmail.com>
2024-06-17 16:00:03 +0530

    done

--- Backend/src/models/Challenge.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const challengeSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: Number, required: true }, // in days
-});
-
-module.exports = mongoose.model('Challenge', challengeSchema);
