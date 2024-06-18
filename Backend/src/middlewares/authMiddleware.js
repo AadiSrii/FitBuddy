5f8ba5c73dd14d56da7606e7687c4f7fe29a9567
tusharbisht16 <tusharbisht0216@gmail.com>
2024-06-18 16:09:32 +0530

    done done

--- Backend/src/middlewares/authMiddleware.js
+++ Backend/src/middlewares/authMiddleware.js
@@ -6,11 +6,11 @@ dotenv.config();
 const authMiddleware = (...allowedRoles) => (req, res, next) => {
 
   const token = req.header('x-auth-token');
-  console.log(token);
+ 
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
-  console.log("hii")
+
   try {
     const decoded =  jwt.verify(token, process.env.JWT_SECRET);
     console.log(decoded)
@@ -18,7 +18,7 @@ const authMiddleware = (...allowedRoles) => (req, res, next) => {
       return res.status(403).json({ msg: 'Unauthorized' });
     }
     req.user = decoded.user;
-    console.log("hii")
+  
     next();
   } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
